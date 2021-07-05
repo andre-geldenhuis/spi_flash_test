8b1fd03580e77ac3ed5500efd2f5ea1973dc41aa
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2021-07-05 14:03:53 +1200

    Working SD forma example

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
-// The MIT License (MIT)
-// Copyright (c) 2019 Ha Thach for Adafruit Industries
+// Adafruit SPI Flash FatFs Format Example
+// Author: Tony DiCola
+//
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!  NOTE: YOU WILL ERASE ALL DATA BY RUNNING THIS SKETCH!  !!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//
+// Usage:
+// - Modify the pins and type of fatfs object in the config
+//   section below if necessary (usually not necessary).
+// - Upload this sketch to your M0 express board.
+// - Open the serial monitor at 115200 baud.  You should see a
+//   prompt to confirm formatting.  If you don't see the prompt
+//   close the serial monitor, press the board reset buttton,
+//   wait a few seconds, then open the serial monitor again.
+// - Type OK and enter to confirm the format when prompted.
+// - Partitioning and formatting will take about 30-60 seconds.
+//   Once formatted a message will be printed to notify you that
+//   it is finished.
+//
+#include <SPI.h>
+#include <SdFat.h>
+#include <Adafruit_SPIFlash.h>
 
-#include "SdFat.h"
-#include "Adafruit_SPIFlash.h"
+// Since SdFat doesn't fully support FAT12 such as format a new flash
+// We will use Elm Cham's fatfs f_mkfs() to format
+#include "ff.h"
+#include "diskio.h"
+
+// up to 11 characters
+#define DISK_LABEL    "EXT FLASH"
 
 // Uncomment to run example with custom SPI and SS e.g with FRAM breakout
 // #define CUSTOM_CS   A5
@@ -24,10 +50,10 @@
   // - EXTERNAL_FLASH_USE_CS/EXTERNAL_FLASH_USE_SPI
   #if defined(EXTERNAL_FLASH_USE_QSPI)
     Adafruit_FlashTransport_QSPI flashTransport;
-
+  
   #elif defined(EXTERNAL_FLASH_USE_SPI)
     Adafruit_FlashTransport_SPI flashTransport(EXTERNAL_FLASH_USE_CS, EXTERNAL_FLASH_USE_SPI);
-
+  
   #else
     #error No QSPI/SPI flash are defined on your board variant.h !
   #endif
@@ -35,78 +61,164 @@
 
 Adafruit_SPIFlash flash(&flashTransport);
 
-
-/*  If you want to use a specific flash device, for example for a custom built board, first look for it in Adafruit_SPIFlash\src\flash_devices.h
- *  If it isn't in there you need to create your own definition like the W25Q80DLX_EXAMPLE example below.
- *  These definitions need to be edited to match information on the data sheet of the flash device that you want to use.
- *  If you are not sure what the manufacture ID, memory type and capacity values should be, try running the sketch anyway and look at the serial output
- *  The flash device will report these values to you as a single hexadecimal value (the JDEC ID)
- *  For example, the first device on the list - the W25Q80DLX - will report its JDEC ID as 0xef4014, which is made of these three values:
- *  manufacturer_id = 0xef
- *  memory_type     = 0x40
- *  capacity        = 0x14
- *  With this macro properly defined you can then create an array of device definitions as shown below, this can include any from the list of devices in flash_devices.h, and any you define yourself here
- *  You need to update the variable on line 71 to reflect the number of items in the array
- *  You also need to uncomment line 84 and comment out line 81 so this array will be passed to the flash memory driver. 
- */
-//Example of a user defined flash memory device:
-//#define W25Q80DLX_EXAMPLE                                                               \
-//  {                                                                            \
-//    .total_size = (1 << 20), /* 1 MiB */                                       \
-//        .start_up_time_us = 5000, .manufacturer_id = 0xef,                     \
-//    .memory_type = 0x40, .capacity = 0x14, .max_clock_speed_mhz = 80,         \
-//    .quad_enable_bit_mask = 0x02, .has_sector_protection = false,              \
-//    .supports_fast_read = true, .supports_qspi = true,                         \
-//    .supports_qspi_writes = false, .write_status_register_split = false,       \
-//    .single_status_byte = false, .is_fram = false,                             \
-//  }
-
-/*
- * Create an array of data structures and fill it with the settings we defined above.
- * We are using two devices, but more can be added if you want.
- */
-//static const SPIFlash_Device_t my_flash_devices[] = {
-//    W25Q80DLX_EXAMPLE,
-//};
-/*
- * Specify the number of different devices that are listed in the array we just created. If you add more devices to the array, update this value to match.
- */
-//const int flashDevices = 1;
-#define W25Q128JV_SQ                                                           \
-  {                                                                            \
-    .total_size = (1UL << 24), /* 16 MiB */                                    \
-        .start_up_time_us = 5000, .manufacturer_id = 0xef,                     \
-    .memory_type = 0x40, .capacity = 0x18, .max_clock_speed_mhz = 133,         \
-    .quad_enable_bit_mask = 0x02, .has_sector_protection = false,              \
-    .supports_fast_read = true, .supports_qspi = true,                         \
-    .supports_qspi_writes = true, .write_status_register_split = false,        \
-    .single_status_byte = false, .is_fram = false,                             \
-  }
-
 static const SPIFlash_Device_t my_flash_devices[] = {
    W25Q128JV_SQ,
 };
 const int flashDevices = 1;
 
-// the setup function runs once when you press reset or power the board
-void setup()
+
+// file system object from SdFat
+FatFileSystem fatfs;
+
+// Elm Cham's fatfs objects
+FATFS elmchamFatfs;
+uint8_t workbuf[4096]; // Working buffer for f_fdisk function.
+
+
+//--------------------------------------------------------------------+
+// fatfs diskio
+//--------------------------------------------------------------------+
+extern "C"
 {
+
+DSTATUS disk_status ( BYTE pdrv )
+{
+  (void) pdrv;
+	return 0;
+}
+
+DSTATUS disk_initialize ( BYTE pdrv )
+{
+  (void) pdrv;
+	return 0;
+}
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+  (void) pdrv;
+	return flash.readBlocks(sector, buff, count) ? RES_OK : RES_ERROR;
+}
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{
+  (void) pdrv;
+  return flash.writeBlocks(sector, buff, count) ? RES_OK : RES_ERROR;
+}
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+  (void) pdrv;
+
+  switch ( cmd )
+  {
+    case CTRL_SYNC:
+      flash.syncBlocks();
+      return RES_OK;
+
+    case GET_SECTOR_COUNT:
+      *((DWORD*) buff) = flash.size()/512;
+      return RES_OK;
+
+    case GET_SECTOR_SIZE:
+      *((WORD*) buff) = 512;
+      return RES_OK;
+
+    case GET_BLOCK_SIZE:
+      *((DWORD*) buff) = 8;    // erase block size in units of sector size
+      return RES_OK;
+
+    default:
+      return RES_PARERR;
+  }
+}
+
+}
+
+void setup() {
+  // Initialize serial port and wait for it to open before continuing.
   Serial.begin(115200);
-
-
-  while ( !Serial ) delay(100);   // wait for native usb
-
-  Serial.println("Adafruit Serial Flash Info example");
-  flash.begin(my_flash_devices);
+  while (!Serial) delay(100);
   
-  //Using a flash device not already listed? Start the flash memory by passing it the array of device settings defined above, and the number of elements in the array.
-  //flash.begin(my_flash_devices, flashDevices);
+  Serial.println("Adafruit SPI Flash FatFs Format Example");
 
-  Serial.print("JEDEC ID: 0x"); Serial.println(flash.getJEDECID(), HEX);
-  Serial.print("Flash size: "); Serial.print(flash.size() / 1024); Serial.println(" KB");
+  // Initialize flash library and check its chip ID.
+  if (!flash.begin(my_flash_devices)) {
+    Serial.println("Error, failed to initialize flash chip!");
+    while(1) yield();
+  }
+  Serial.print("Flash chip JEDEC ID: 0x"); Serial.println(flash.getJEDECID(), HEX);
+
+  // Uncomment to flash LED while writing to flash
+  // flash.setIndicator(LED_BUILTIN, true);
+
+  // Wait for user to send OK to continue.
+  Serial.setTimeout(30000);  // Increase timeout to print message less frequently.
+  do {
+    Serial.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    Serial.println("This sketch will ERASE ALL DATA on the flash chip and format it with a new filesystem!");
+    Serial.println("Type OK (all caps) and press enter to continue.");
+    Serial.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  } while ( !Serial.find("OK"));
+
+  // Call fatfs begin and passed flash object to initialize file system
+  Serial.println("Creating and formatting FAT filesystem (this takes ~60 seconds)...");
+
+  // Make filesystem.
+  FRESULT r = f_mkfs("", FM_FAT | FM_SFD, 0, workbuf, sizeof(workbuf));
+  if (r != FR_OK) {
+    Serial.print("Error, f_mkfs failed with error code: "); Serial.println(r, DEC);
+    while(1) yield();
+  }
+
+  // mount to set disk label
+  r = f_mount(&elmchamFatfs, "0:", 1);
+  if (r != FR_OK) {
+    Serial.print("Error, f_mount failed with error code: "); Serial.println(r, DEC);
+    while(1) yield();
+  }
+
+  // Setting label
+  Serial.println("Setting disk label to: " DISK_LABEL);
+  r = f_setlabel(DISK_LABEL);
+  if (r != FR_OK) {
+    Serial.print("Error, f_setlabel failed with error code: "); Serial.println(r, DEC);
+    while(1) yield();
+  }
+
+  // unmount
+  f_unmount("0:");
+
+  // sync to make sure all data is written to flash
+  flash.syncBlocks();
+  
+  Serial.println("Formatted flash!");
+
+  // Check new filesystem
+  if (!fatfs.begin(&flash)) {
+    Serial.println("Error, failed to mount newly formatted filesystem!");
+    while(1) delay(1);
+  }
+
+  // Done!
+  Serial.println("Flash chip successfully formatted with new empty filesystem!");
 }
 
-void loop()
-{
-  // nothing to do
+void loop() {
+  // Nothing to be done in the main loop.
 }
+
+
